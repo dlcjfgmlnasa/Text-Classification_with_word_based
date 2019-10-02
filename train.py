6afbafe27f8c2168a17dd84f5009687ecf2796d5
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2019-10-03 02:18:01 +0900

    코드 리팩토링 - Trainer 클래스 생성

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 import os
 import torch
-import pickle
 import argparse
+import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as f
 from common import accuracy
 from torch.utils.data import DataLoader
-from data_helper import TextDataSet, Dictionary
+from data_helper import TextDataSet, Dictionary, DataPreProcessing
 from tensorboardX import SummaryWriter
 
 
@@ -16,163 +17,277 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_path', type=str,
+    parser.add_argument('--origin_train_path', type=str,
                         default=os.path.join('./dataset', 'ratings_train.txt'))
     parser.add_argument('--dictionary_path', type=str,
-                        default=os.path.join('./dataset', 'dictionary', 'word2idx.dic'))
-    parser.add_argument('--model', type=str,
-                        default='TextCNN',
-                        choices=['TextCNN'])
+                        default=os.path.join('./dataset', 'dictionary'))
+    parser.add_argument('--data_preprocessing_file_dir', type=str,
+                        default=os.path.join('./dataset', 'preprocessing'))
 
-    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--model', type=str,
+                        default='TextRNN',
+                        choices=['TextCNN', 'TextRNN'])
+
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--batch_size', type=int, default=500)
     parser.add_argument('--classes', type=int, default=2)
     parser.add_argument('--seq_len', type=int, default=20)
-    parser.add_argument('--batch_size', type=int, default=200)
-    parser.add_argument('--out_channels', type=int, default=20)
-    parser.add_argument('--embedding_dim', type=int, default=256)
+    parser.add_argument('--embedding_dim', type=int, default=512)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--data_split_rate', type=float, default=0.2)
+
+    # TextCNN
+    parser.add_argument('--out_channels', type=int, default=50)
+    parser.add_argument('--dropout_rate', type=float, default=0.8)
     parser.add_argument('--n_grams', type=list, default=[2, 3, 4])
+
+    # TextRNN
+    parser.add_argument('--rnn_dim', type=int, default=50)
+    parser.add_argument('--rnn_num_layer', type=int, default=2)
+    parser.add_argument('--rnn_bidirectional', type=bool, default=True)
 
     parser.add_argument('--print_summary_step', type=int, default=50)
     parser.add_argument('--print_step', type=int, default=50)
+    parser.add_argument('--print_val_step', type=int, default=300)
 
     parser.add_argument('--summary_store', type=str,
-                        default=os.path.join('./store', 'text_cnn', 'log'))
+                        default=os.path.join('./store', 'text_rnn', 'log'))
     parser.add_argument('--model_store', type=str,
-                        default=os.path.join('./store', 'text_cnn', 'model'))
+                        default=os.path.join('./store', 'text_rnn', 'model'))
     return parser.parse_args()
 
 
-def get_model(model_type, param) -> torch.nn.Module:
-    if model_type == 'TextCNN':
-        from models.textcnn import TextCNN
-        model = TextCNN(**param)
-        return model
-    else:
-        raise NotImplemented()
+class Trainer(object):
+    def __init__(self, argument):
+        self.argument = argument
+        self.dictionary = self.load_dictionary()
+        self.word2idx = self.dictionary.load_word2idx()
+        self.dp = self.data_preprocessing()
+        self.model = self.get_model()
+        self.summary = SummaryWriter(self.argument.summary_store)
+        self.criterion = nn.BCELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.argument.learning_rate)
+        self.train()
 
+    def train(self):
+        # Training Start
+        self.model.train()
+        self.model.to(device)
 
-def load_dictionary(dictionary_path):
-    word2idx = pickle.load(open(dictionary_path, 'rb'))
-    return word2idx
+        # Checking Model Store
+        if not os.path.exists(self.argument.model_store):
+            os.makedirs(self.argument.model_store)
 
+        print('Training Start...')
 
-def data_loader(data_path: str, dictionary: Dictionary, seq_len: int, batch_size: int, shuffle_flag: bool) \
-        -> DataLoader:
-    # Dataset & DataLoader
-    dataset = TextDataSet(
-        data_path=data_path,
-        dictionary=dictionary,
-        seq_len=seq_len
-    )
-    dataloader = DataLoader(
-        dataset=dataset,
-        batch_size=batch_size,
-        shuffle=shuffle_flag
-    )
-    return dataloader
+        total_count = 0
+        for epoch in range(self.argument.epochs):
+            epoch_count = 0
+            average_loss = 0
+            average_accuracy = 0
+            for i, data in enumerate(self.train_dataloader()):
+                feature, label = data
+                # NetWork Forward
+                output = self.model(feature)
 
+                # Calculation Accuracy & Loss
+                loss, acc = self.get_accuracy_loss(inputs=output, target=label)
 
-def main(argument):
-    # Loading Dictionary
-    if not os.path.exists(argument.dictionary_path):
-        base_path = os.path.dirname(argument.dictionary_path)
-        dictionary = Dictionary(
-            data_path=argument.train_path,
-            dict_path=base_path
-        )
-    else:
-        dictionary = Dictionary()
+                # Optimizer & Network Backward
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
-    # Loading Word2Idx
-    word2idx = dictionary.load_word2idx()
-    word2idx_len = len(word2idx)
+                # Printing...
+                # - Print Log
+                if i % self.argument.print_step == 0:
+                    print('[Train] epoch : {0: 3d} \t i : {1:3d} \t loss : {2:2.6f} \t accuracy : {3:.5f}'.
+                          format(epoch, i, loss.item(), acc.item()))
 
-    # Tensorboard Summary
-    summary = SummaryWriter(argument.summary_store)
+                # - Print Summary
+                if i % self.argument.print_summary_step == 0:
+                    self.summary.add_scalar('train/loss', loss.item(), total_count)
+                    self.summary.add_scalar('train/accuracy', acc.item(), total_count)
 
-    # Checking Model Store
-    if not os.path.exists(argument.model_store):
-        os.makedirs(argument.model_store)
+                # - Calculation Validation
+                if i % self.argument.print_val_step == 0:
+                    val_loss, val_acc = self.val()
+                    print('[ Val ] epoch : {0: 3d} \t i : {1:3d} \t loss : {2:2.6f} \t accuracy : {3:.5f}'.
+                          format(epoch, i, loss.item(), acc.item()))
+                    self.summary.add_scalar('val/loss', val_loss, total_count)
+                    self.summary.add_scalar('val/accuracy', val_acc, total_count)
 
-    # Train Dataset & DataLoader
-    train_dataloader = data_loader(argument.train_path, dictionary,
-                                   seq_len=argument.seq_len, batch_size=argument.batch_size, shuffle_flag=True)
+                average_loss += loss.item()
+                epoch_count += 1
+                total_count += 1
 
-    # Get Model
-    parameter = {
-        'seq_len': argument.seq_len,
-        'word_size': word2idx_len,
-        'embedding_dim': argument.embedding_dim,
-        'out_channels': argument.out_channels,
-        'classes': argument.classes,
-        'n_grams': argument.n_grams,
-        'padding_idx': word2idx['__PAD__']
-    }
-    model = get_model(
-        model_type=argument.model,
-        param=parameter
-    )
+            # Average Loss & Accuracy
+            average_loss /= epoch_count
+            average_accuracy /= epoch_count
 
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = nn.CrossEntropyLoss(ignore_index=word2idx['__PAD__'])
+            # Saving model...
+            self.save_model(
+                epoch=epoch,
+                average_loss=average_loss,
+                average_accuracy=average_loss
+            )
 
-    print('Training Start...')
-
-    total_count = 0
-    for epoch in range(argument.epochs):
-        epoch_count = 0
+    def val(self):
+        count = 0
         average_loss = 0
-        for i, data in enumerate(train_dataloader):
-            feature, label = data
+        average_acc = 0
 
-            output = model(feature)
+        with torch.no_grad():
+            for feature, label in self.val_dataloader():
+                output = self.model(feature)
+                loss, acc = self.get_accuracy_loss(inputs=output, target=label)
+                average_loss += loss.item()
+                average_acc += acc.item()
+                count += 1
+        average_loss /= count
+        average_acc /= count
+        return average_loss, average_acc
 
-            output = output.to(device=device)
-            label = label.to(device=device, dtype=torch.int64)
+    def get_accuracy_loss(self, inputs, target):
+        target_one_hot = f.one_hot(target, num_classes=self.argument.classes).float()
 
-            # Calculation Accuracy & Loss
-            loss = criterion(input=output, target=label)
-            acc = accuracy(inputs=output, target=label)
+        # Calculation Accuracy & Loss
+        loss = self.criterion(input=inputs, target=target_one_hot)
+        acc = accuracy(inputs=inputs, target=target)
+        return loss, acc
 
-            # Optimizer
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    def train_dataloader(self):
+        # Train DataSet & DataLoader
+        train_dataset = TextDataSet(
+            path=os.path.join(self.argument.data_preprocessing_file_dir, 'preprocessing_train.json'),
+            device=device
+        )
+        train_dataloader = DataLoader(
+            dataset=train_dataset,
+            batch_size=self.argument.batch_size,
+            shuffle=True
+        )
+        return train_dataloader
 
-            # Printing...
-            # - Print Log
-            if i % argument.print_step == 0:
-                print('epoch : {0: 3d} \t i : {1:3d} \t loss : {2:5f} \t accuracy : {3:5f}'.
-                      format(epoch, i, loss.item(), acc.item()))
+    def val_dataloader(self):
+        # Validation Dataset & DataLoader
+        val_dataset = TextDataSet(
+            path=os.path.join(self.argument.data_preprocessing_file_dir, 'preprocessing_val.json'),
+            device=device
+        )
+        val_dataloader = DataLoader(
+            dataset=val_dataset,
+            batch_size=1000,
+            shuffle=False
+        )
+        return val_dataloader
 
-            # - Print Summary
-            if i % argument.print_summary_step == 0:
-                summary.add_scalar('loss', loss.item(), total_count)
-                summary.add_scalar('accuracy', acc.item(), total_count)
+    def data_preprocessing(self):
+        # Loading Dataset & Split Train / Validation
+        frame = self.load_dataset()
+        train_df, val_df = self.split_train_val_dataset(frame)
 
-            average_loss += loss.item()
-            epoch_count += 1
-            total_count += 1
+        # Get DataPreProcessing Class
+        dp = DataPreProcessing(
+            dictionary=self.dictionary,
+            seq_len=self.argument.seq_len
+        )
+        if not os.path.exists(self.argument.data_preprocessing_file_dir):
+            # if not exist preprocessing folder
+            # => Train
+            dp.start(
+                df=train_df,
+                target_path=self.argument.data_preprocessing_file_dir,
+                target_filename='preprocessing_train.json',
+            )
+            # => Val
+            dp.start(
+                df=val_df,
+                target_path=self.argument.data_preprocessing_file_dir,
+                target_filename='preprocessing_val.json',
+            )
+        return dp
 
-        # Average Loss...
-        average_loss /= epoch_count
+    def load_dataset(self):
+        # Loading Original Dataset
+        dataset = pd.DataFrame([
+            (line.split('\t')[1], line.split('\t')[2])
+            for line in open(self.argument.origin_train_path, 'r', encoding='utf-8').readlines()[1:]
+        ], columns=['document', 'label'])
+        dataset = dataset.dropna(how='any')
+        dataset.document = dataset.document.str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")
+        return dataset
 
+    def split_train_val_dataset(self, frame):
+        # Split Train / Validation
+        split_rate = self.argument.data_split_rate
+        split_point = int(len(frame) * (1 - split_rate))
+        train_df = frame.iloc[:split_point]
+        val_df = frame.iloc[split_point:]
+        return train_df, val_df
+
+    def load_dictionary(self):
+        # Loading Dictionary
+        dictionary_path = self.argument.dictionary_path
+        if not os.path.exists(dictionary_path):
+            # if not exist dictionary (word2count, word2idx, idx2word)
+            dictionary = Dictionary(
+                dict_path=dictionary_path
+            )
+            dictionary.make_dict(self.argument.origin_train_path)
+        else:
+            dictionary = Dictionary(
+                dict_path=self.argument.dictionary_path
+            )
+        return dictionary
+
+    def get_model(self):
+        model_type = self.argument.model
+        parameter = self.get_parameter()
+        if model_type == 'TextCNN':
+            from models.textcnn import TextCNN
+            return TextCNN(**parameter)
+        elif model_type == 'TextRNN':
+            from models.textrnn import TextRNN
+            return TextRNN(**parameter)
+        else:
+            raise NotImplemented()
+
+    def get_parameter(self):
+        argument = self.argument
+        model_type = argument.model
+        if model_type == 'TextCNN':
+            parameter = {
+                'seq_len': argument.seq_len, 'word_size': len(self.word2idx),
+                'embedding_dim': argument.embedding_dim, 'out_channels': argument.out_channels,
+                'classes': argument.classes, 'n_grams': argument.n_grams,
+                'padding_idx': self.word2idx['__PAD__'], 'dropout_rate': argument.dropout_rate
+            }
+            return parameter
+        elif model_type == 'TextRNN':
+            parameter = {
+                'word_size': len(self.word2idx), 'embedding_dim': argument.embedding_dim, 'rnn_dim': argument.rnn_dim,
+                'num_layer': argument.rnn_num_layer, 'classes': argument.classes,
+                'padding_idx': self.word2idx['__PAD__'], 'bidirectional': argument.rnn_bidirectional
+            }
+            return parameter
+
+    def save_model(self, epoch, average_loss, average_accuracy):
         # Saving model...
-        model_path = os.path.join(argument.model_store, '{0:04d}.pth'.format(epoch))
+        model_path = os.path.join(self.argument.model_store, '{0:04d}.pth'.format(epoch))
         torch.save({
             'epoch': epoch,
-            'seq_len': argument.seq_len,
-            'model_type': argument.model,
-            'dictionary_path': argument.dictionary_path,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': average_loss,
-            'parameter': parameter
+            'seq_len': self.argument.seq_len,
+            'model_type': self.argument.model,
+            'dictionary_path': self.argument.dictionary_path,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'epoch_average_loss': average_loss,
+            'epoch_average_accuracy': average_accuracy,
+            'parameter': self.get_parameter()
         }, model_path)
-
-    print('Complete...')
 
 
 if __name__ == '__main__':
     args = get_args()
-    main(args)
+    Trainer(args)
